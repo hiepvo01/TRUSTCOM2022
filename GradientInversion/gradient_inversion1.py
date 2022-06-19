9ff285a1b6fc5c79f8ce93195b96cfb33ca108e0
hiepvo01 <vohi0311@gmail.com>
2022-06-19 16:34:51 +1000

    working with custom data

--- GradientInversion/gradient_inversion1.py
+++ GradientInversion/gradient_inversion1.py
@@ -98,23 +98,20 @@ from aijack.attack import GradientInversion_Attack
 class LeNet(nn.Module):
     def __init__(self, channel=1, hideen=588, num_classes=10):
         super(LeNet, self).__init__()
+        act = nn.Sigmoid
         self.body = nn.Sequential(
-            nn.Conv2d(channel, 32, kernel_size=(3,3), stride=(2,2), padding=1),
-            nn.LeakyReLU(negative_slope=0.2),
-            nn.Dropout(0.5),
-            nn.Conv2d(32, 64, kernel_size=(3,3), padding=1),
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(negative_slope=0.2),
-            nn.Dropout(0.5),
-            nn.Conv2d(64, 128, kernel_size=(3,3), stride=(2,2), padding=1),
-            nn.BatchNorm2d(128),
-            nn.LeakyReLU(negative_slope=0.2),
-            nn.Dropout(0.5),
-            nn.Flatten()
+            nn.Conv2d(channel, 12, kernel_size=5, padding=5 // 2, stride=2),
+            nn.BatchNorm2d(12),
+            act(),
+            nn.Conv2d(12, 12, kernel_size=5, padding=5 // 2, stride=2),
+            nn.BatchNorm2d(12),
+            act(),
+            nn.Conv2d(12, 12, kernel_size=5, padding=5 // 2, stride=1),
+            nn.BatchNorm2d(12),
+            act(),
         )
         self.fc = nn.Sequential(
-            nn.Linear(6272, num_classes)
-            # nn.Linear(hideen, num_classes)
+            nn.Linear(hideen, num_classes)
         )
 
     def forward(self, x):
@@ -123,7 +120,7 @@ class LeNet(nn.Module):
         out = self.fc(out)
         return out
 
-batch_size = 4
+batch_size = 2
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 train_loader = [torch.utils.data.DataLoader(x, batch_size=batch_size, shuffle=True) for x in torch.load('../data/MNIST.pth')]
@@ -149,11 +146,11 @@ for i in range(len(train_loader)):
         received_gradients = torch.autograd.grad(loss, net.parameters())
         received_gradients = [cg.detach() for cg in received_gradients]
 
-        gradinversion = GradientInversion_Attack(net, (1, 28, 28), num_iteration=2500,
+        gradinversion = GradientInversion_Attack(net, (1, 28, 28), num_iteration=1200,
                                             lr=1e2, log_interval=0,
                                             optimizer_class=torch.optim.SGD,
                                             distancename="l2", optimize_label=False,
-                                            bn_reg_layers=[net.body[4], net.body[8]],
+                                            bn_reg_layers=[net.body[1], net.body[4], net.body[8]],
                                             group_num = 3,
                                             tv_reg_coef=0.00, l2_reg_coef=0.0001,
                                             bn_reg_coef=0.001, gc_reg_coef=0.001, device=device)
@@ -169,5 +166,5 @@ for i in range(len(train_loader)):
             client_label.append(label)
     generated_images.append(client_img)
     generated_labels.append(client_label)
-torch.save(generated_images, 'generated_images_test1.pth')
-torch.save(generated_labels, 'generated_labels_test1.pth')
+torch.save(generated_images, 'generated_images_train1.pth')
+torch.save(generated_labels, 'generated_labels_train1.pth')
